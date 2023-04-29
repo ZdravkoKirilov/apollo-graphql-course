0a5103da03f8e4a0a65f15d5e80289421455d67c
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2023-04-29 18:42:18 +0300

    add mocks

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+const gql = require("graphql-tag");
 
 const typeDefs = gql`
   type Query {
@@ -31,4 +31,4 @@ const typeDefs = gql`
   }
 `;
 
-export default typeDefs;
+module.exports = typeDefs;
